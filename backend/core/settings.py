b87d3f7ede5afafa8853aec3e2caa249e59c8203
Kedhareswari1701 <kethana2022@gmail.com>
2026-08-31 17:54:29 +0530

    Fix PostgreSQL psycopg dependency

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -217,3 +217,15 @@ LOGGING = {
         },
     },
 }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
